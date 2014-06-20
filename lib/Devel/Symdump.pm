917268bed2d68ed7e2021688f790e43c707a978b
Andreas J. König <andreas.koenig.7os6vvqr@franz.ak.mind.de>
2014-06-20 19:44:30 +0000

    Import of ANDK/Devel-Symdump-2.12 from CPAN.
    gitpan-cpan-distribution: Devel-Symdump
    gitpan-cpan-version:      2.12
    gitpan-cpan-path:         ANDK/Devel-Symdump-2.12.tar.gz
    gitpan-cpan-author:       ANDK
    gitpan-cpan-maturity:     released

--- lib/Devel/Symdump.pm
+++ lib/Devel/Symdump.pm
@@ -5,7 +5,7 @@ use Carp ();
 use strict;
 use vars qw($Defaults $VERSION *ENTRY $MAX_RECURSION);
 
-$VERSION = '2.11';
+$VERSION = '2.12';
 $MAX_RECURSION = 97;
 
 $Defaults = {
@@ -450,7 +450,8 @@ Devel::Symdump package and turns them into functions.
 
 =head1 SEE ALSO
 
-Package::Stash
+Routines for manipulating stashes: C<Package::Stash>; to work with
+lexicals: C<PadWalker>.
 
 =head1 AUTHORS
 
